e7524474c04e2e83851b7014b7ef03282e133693
Leo Dion <leogdion@brightdigit.com>
2019-05-19 15:37:29 -0400

    struct vs classes

--- workshop app/ViewController.swift
+++ workshop app/ViewController.swift
@@ -8,6 +8,19 @@
 
 import UIKit
 
+struct ValuesStruct {
+  var array : [Int]
+  
+}
+
+class ValuesClass {
+  var array : [Int]
+  
+  init (array: [Int]) {
+    self.array = array
+  }
+}
+
 class ViewController: UIViewController {
   let numbers = [Int]()
   @IBOutlet weak var structLabel : UILabel!
@@ -16,8 +29,17 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
-    structLabel.text = numbers.first?.description ?? "No First Value"
+    var stct = ValuesStruct(array: [1,2,3,4,5])
+    var cls = ValuesClass(array: [1,2,3,4,5])
     
+    var newStct = stct
+    newStct.array = [2,4,8]
+    
+    var newCls = cls
+    cls.array = [2,4,8]
+    
+    structLabel.text = stct.array.description
+    classLabel.text = cls.array.description
   }
 
 
