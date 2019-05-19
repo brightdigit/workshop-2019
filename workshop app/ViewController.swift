b9cec1bb52796e5e2bfa3db02c663c95161cf605
Leo Dion <leogdion@brightdigit.com>
2019-05-19 15:14:43 -0400

    crashing optional

--- workshop app/ViewController.swift
+++ workshop app/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet weak var structLabel : UILabel!
+  @IBOutlet weak var classLabel : UILabel!
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    
+    debugPrint(classLabel.text)
   }
 
 
